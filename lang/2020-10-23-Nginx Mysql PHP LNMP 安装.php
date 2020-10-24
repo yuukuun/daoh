21fd1085a1eb7b8ce623498a6c4025dbfb2741d1
yuukuun <qingkele@gmail.com>
2020-10-24 17:56:02 +0800

    2020.10.24-17:56

--- lang/2020-10-23-Nginx Mysql PHP LNMP 安装.php
+++ lang/2020-10-23-Nginx Mysql PHP LNMP 安装.php
@@ -114,6 +114,19 @@ for($i=0;$i<count($cbody);$i++){ //count函数就是获取数组的长度的，�
 </code></pre></div>
 
 
+<div class="row"><span class="txt">安装PHP7.3：</span></div>
+<div class="row"><pre><code>
+<?php
+// install SSL addHost
+$file = '../shell/insPHP73.sh'; //先读取文件
+$cbody = file($file); //file（）函数作用是返回一行数组，txt里有三行数据，因此一行被识别为一个数组，三行被识别为三个数组
+for($i=0;$i<count($cbody);$i++){ //count函数就是获取数组的长度的，长度为3 因为一行被识别为一个数组 有三行
+  echo $cbody[$i]; //最后是循环输出每个数组，在每个数组输出完毕后 ，输出一个换行，这样就可以达到换行效果
+}
+?>
+</code></pre></div>
+
+
 <div class="row"><span class="txt">添加域名和SSL证书：</span></div>
 <div class="row"><pre><code>
 <?php
@@ -138,6 +151,7 @@ for($i=0;$i<count($cbody);$i++){ //count函数就是获取数组的长度的，�
 }
 ?>
 </code></pre></div>
+
 <div class="row"><pre><code>
 <?php
 // install SSL addHost
@@ -150,17 +164,7 @@ for($i=0;$i<count($cbody);$i++){ //count函数就是获取数组的长度的，�
 </code></pre></div>
 
 
-<div class="row"><span class="txt">安装PHP7.3：</span></div>
-<div class="row"><pre><code>
-<?php
-// install SSL addHost
-$file = '../shell/insPHP73.sh'; //先读取文件
-$cbody = file($file); //file（）函数作用是返回一行数组，txt里有三行数据，因此一行被识别为一个数组，三行被识别为三个数组
-for($i=0;$i<count($cbody);$i++){ //count函数就是获取数组的长度的，长度为3 因为一行被识别为一个数组 有三行
-  echo $cbody[$i]; //最后是循环输出每个数组，在每个数组输出完毕后 ，输出一个换行，这样就可以达到换行效果
-}
-?>
-</code></pre></div>
+
 <div class="row"><pre><code>LANG</code></pre></div>
 <!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
 <!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
