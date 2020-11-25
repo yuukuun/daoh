190017e366d9a4acf52a3d2c59df71e2516b0763
yuukuun <qingkele@gmail.com>
2020-11-25 10:14:27 +0800

    2020.11.25-10:14

--- new/index.php
+++ new/index.php
@@ -1,30 +1,96 @@
-<?php 
-require 'model/SourceData.php';   
-require 'cont/show.php';   
+<?php  
+require 'model/data.php'; 
 
+?>
 
+<!doctype html>
+<html lang="en">
+<!--------------------------------------------------头部-------------------------------------------------------->
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-$head = 'view/head.html'; //先读取文件
-$temp = file($head); //file（）函数作用是返回一行数组，txt里有三行数据。
-for($i=0;$i<count($temp);$i++){ //count函数就是获取数组的长度的，长度为3 因为一行被识别为一个数组 有三行
-    echo $temp[$i]; //最后是循环输出每个数组
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="css/offcanvas.css" rel="stylesheet">
+    <title>软件</title>
+  </head>
+<!--------------------------------------------------导航-------------------------------------------------------->
+  <body class="bg-light" id="ups" name="ups">
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+  <a class="navbar-brand mr-auto mr-lg-0" href="#">&nbsp;</a>
+  <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+    <!--   <li class="nav-item"><a class="nav-link" href="shoes.html">鞋</a></li> -->
+ 
+<?php  
+
+$num = count($arr);
+for ($i=0; $i < $num; $i++) { 
+	// print_r($arr[$i][3]);
+	echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"shoes.html\">".$arr[$i][3];
+	echo "<br>";
 }
 
-NavBar_two($music_data);
-
-$bodystart = 'view/bodystart.html'; //先读取文件
-$temp = file($bodystart); //file（）函数作用是返回一行数组，txt里有三行数据。
-for($i=0;$i<count($temp);$i++){ //count函数就是获取数组的长度的，长度为3 因为一行被识别为一个数组 有三行
-    echo $temp[$i]; //最后是循环输出每个数组
-}
-
-ShowPage($music_data);
-
-$bodyend = 'view/bodyend.html'; //先读取文件
-$temp = file($bodyend); //file（）函数作用是返回一行数组，txt里有三行数据。
-for($i=0;$i<count($temp);$i++){ //count函数就是获取数组的长度的，长度为3 因为一行被识别为一个数组 有三行
-    echo $temp[$i]; //最后是循环输出每个数组
-}
+?>
 
 
-?> 
+    </ul>
+  </div>
+</nav>
+<!--------------------------------------------------二级导航-------------------------------------------------------->
+
+<!--------------------------------------------------内容-------------------------------------------------------->
+<main role="main" class="container">
+<!---------------------------------------------------------------------------------------------------------------------------->
+<!---------------------------------------------------------------------------------------------------------------------------->
+<div class="row"><div class="col-xs-12 col-md-12 btit">系统</div></div>
+<div class="row">
+
+<div class="icon col-3 col-md-1 "><a href="http://laomaotao.org/" target="_blank"><img src="soft/laoshantao.png" class="img-fluid rounded" alt="Responsive image"></a></div>  
+<div class="icon col-3 col-md-1 ibor"><a href="./soft/FbinstTool.zip" target="_blank"><img src="soft/FbinstTool.png" class="img-fluid rounded" alt="Responsive image"></a></div>  
+<div class="icon col-3 col-md-1 ibor"><a href="./soft/FbinstTool.zip" target="_blank"><img src="soft/FbinstTool.png" class="img-fluid rounded" alt="Responsive image"></a></div>  
+
+
+
+</div><hr>
+<h1>asdsadasdas</h1>
+
+
+
+<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+<div class="row hou"><hr></div>
+</main>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="view/css/offcanvas.js"></script>
+<!-- <div class="goTop" id="js-go_top"><a href="#ups"><img src="view/css/icon_top.png" alt="回到顶部图片"></a></div> -->
+<script src="view/css/GoToTop.js"></script>
+
+  </body>
+</html>
