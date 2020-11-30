62a4feb65ad4be1c25f0876ef6c257737862f81e
yuukuun <qingkele@gmail.com>
2020-11-30 08:17:50 +0800

    2020.11.30-08:17

--- shell/insNginxConf.sh
+++ shell/insNginxConf.sh
@@ -1,6 +1,8 @@
 location /soft {
     sub_filter moru.gq qiqing.cf;
     sub_filter_once off;
+    proxy_ssl_name $host;
+	proxy_ssl_server_name on;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header Referer https://moru.gq;
