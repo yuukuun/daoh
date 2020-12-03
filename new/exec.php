17c01ac39850ad9a065e5d87e91e189a8f91cde3
yuukuun <qingkele@gmail.com>
2020-12-03 16:43:38 +0800

    2020.12.03-16:43

--- new/exec.php
+++ new/exec.php
@@ -29,7 +29,10 @@ function  fors($arr) {
 function  show_cont_tit($path) {
  	echo "<div class=\"row\"><div class=\"col-xs-12 col-md-12 btit\">".$path."</div></div> <div class=\"row\">";
  	foreach (glob($path."/*.html") as $pathname) {
-		echo "<div class=\"col-12 col-md-6 tit\"><a href=\"page.php?tit=".$pathname."\">".$pathname."</a></div>";
+ 		$title = strstr($pathname,'.',true);
+ 		$title = strstr($title,'/');
+ 		$title = str_replace('/','',$title);
+		echo "<div class=\"col-12 col-md-6 tit\"><a href=\"page.php?tit=".$pathname."\">".$title."</a></div>";
 	}
   	echo "</div>";
 }
